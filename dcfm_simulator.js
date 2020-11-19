6f3ecf4a9481f457b1cae757128db3593f41b794
Samer Al-khateeb <sxalkhateeb@ualr.edu>
2020-11-19 17:57:13 -0600

    added simulation

--- dcfm_simulator.js
+++ dcfm_simulator.js
@@ -5,10 +5,35 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////// Code for Running the DCFM Simulation Many Times////////////////////////// 
+
+function simulationResultsSummmary(resultsArray, numOfSims)
+  {
+    var succcess_counter = 0;
+    var fail_counter = 0;
+    var succcess_counter_Percent = 0;
+
+    for (var i = 0; i < resultsArray.length; ++i )
+    {
+        if (resultsArray[i] == "Success")
+          {
+            succcess_counter = succcess_counter + 1;
+          }
+        else
+          {
+            fail_counter = fail_counter + 1;
+          }
+    }
+
+    succcess_counter_Percent = (succcess_counter/numOfSims) * 100;
+
+    output1.innerHTML = succcess_counter_Percent.toFixed(1) + " % of the times DCFM succeeded!";
+  }
+
 function runSim()
   {
+    
     var numofinvprac = parseInt(document.getElementById('invnumofprac').value);  //the number of invited people
-    var T_o = parseFloat(parseFloat(document.getElementById('threshold').value) / 100); // the threshold of DCFM success
+    var T_o = parseFloat(document.getElementById('threshold').value) / 100; // the threshold of DCFM success
     var numofruns = parseInt(document.getElementById('numofsim').value); // number of simulation
     var numofpowact = parseInt(document.getElementById('numofpow').value); // number of powerful actors
 		
@@ -20,45 +45,54 @@ function runSim()
 
           }
 
-   var WCounter = new Array( numofruns );   //declearing WCounter array and initialize all its elements to zero
+    var WCounter = new Array( numofruns );   //declearing WCounter array and initialize all its elements to zero
     for ( var i = 0; i < numofruns; ++i )
-        {WCounter[ i ] = 0 ;}
+        { 
+          WCounter[ i ] = 0 ;
+        }
 
 
-   var PECounter = new Array( numofruns );  //declearing PECounter array and initialize all its elements to zero
+    var PECounter = new Array( numofruns );  //declearing PECounter array and initialize all its elements to zero
     for ( var i = 0; i < numofruns; ++i )
-        {PECounter[ i ] = 0 ;}
+        {
+          PECounter[ i ] = 0 ;
+        }
 
 
-   var AACounter = new Array( numofruns );  //declearing AACounter array and initialize all its elements to zero
+    var AACounter = new Array( numofruns );  //declearing AACounter array and initialize all its elements to zero
     for ( var i = 0; i < numofruns; ++i )
-        {AACounter[ i ] = 0 ;}
+        {
+          AACounter[ i ] = 0 ;
+        }
 
 
-   var participation_rate = new Array( numofruns );  //declearing participation_rate array and initialize all its elements to zero
+    var participation_rate = new Array( numofruns );  //declearing participation_rate array and initialize all its elements to zero
     for ( var i = 0; i < numofruns; ++i )
-        {participation_rate[ i ] = 0 ;}
+        {
+          participation_rate[ i ] = 0;
+        }
 
 
     var result = new Array( numofruns );  //declearing result array and initialize all its elements to empty string
     for ( var i = 0; i < numofruns; ++i )
-        {result[ i ] = '' ;}
+        {
+          result[ i ] = '' ;
+        }
 
 	
-	if (numofpowact > numofinvprac) //check if we have more powerful actors than the number of invited people, the script will not run
-		{	
-			resetAll();
-			window.alert ("the number of Powerful Actors can not be more than the number of Invited People. Please adjust the numbers");
-
-		}
+	  if (numofpowact > numofinvprac) //check if we have more powerful actors than the number of invited people, the script will not run
+		  {	
+			   resetAll();
+			   window.alert("the number of Powerful Actors can not be more than the number of Invited People. Please adjust the numbers");
+		  }
 	
-	else //if the number of powerful actors equal or same as number of invited people then the script will run
-	{
+	  else //if the number of powerful actors equal or same as number of invited people then the script will run
+	    {
 
-  	  for ( var i = 0; i < numofruns; ++i)     // the variable i is used for the number of runs/simulations
-  		{
+  	    for ( var i = 0; i < numofruns; ++i)     // the variable i is used for the number of runs/simulations
+  		   {
 
-        for ( var j = 0; j < numofinvprac; ++j)     // the variable j is used for the number of individuals inserted
+            for ( var j = 0; j < numofinvprac; ++j)     // the variable j is used for the number of individuals inserted
             {
                 var p_interest = parseInt(RandomOneOf(["0", "1"]));
 
@@ -121,11 +155,10 @@ function runSim()
             	
               participation_rate[i] = parseFloat(ACounter[i] / numofinvprac);
               
-              //var succcess_counter = 0;
+              
             	if ( participation_rate[i] > T_o) 
                 	{ 
                   		result[i] = "Success"; 
-                      //succcess_counter = succcess_counter + 1;
                 	}
             	else 
                 	{ 
@@ -134,9 +167,10 @@ function runSim()
 
             }
 
-            //document.getElementById('output1').innerHTML = "There are " + succcess_counter + " DCFM that succeeded out of " + numofruns;
 
-            outputTable ("Simulation Result:", numofinvprac, ACounter, WCounter, PECounter, AACounter, result, document.getElementById('output2'));
+            simulationResultsSummmary(result, numofruns);
+
+            outputTable("Simulation Result:", numofinvprac, ACounter, WCounter, PECounter, AACounter, result, document.getElementById('output2'));
 
   		}
   	}
@@ -145,19 +179,20 @@ function runSim()
 
 
 	////////////////////////// function to print the simulation result as a Table ///////////////////
-  	function outputTable (heading, numofinvprac, ACounter, WCounter, PECounter, AACounter, result, output2)
+  	function outputTable(heading, numofinvprac, ACounter, WCounter, PECounter, AACounter, result, output2)
     	{
+
      		var content = "<h2>" + heading + "</h2> <table border ='1'><thead><th> Simulation Number</th><th> Number of Invited Practitioners </th><th> Number of Acting </th><th> Number of Withdraw </th><th> Number of Power Exchange</th><th> Number of Acting Against</th><th> DCFM Result </th></thead><tbody>";
 
-      		var Alength = result.length; // get array's length once before loop
+      	var Alength = result.length; // get array's length once before loop
    
-        	for ( var i = 0; i < Alength; ++i )   
+        for ( var i = 0; i < Alength; ++i )   
               {
                 content += "<tr><td>" + i + "</td><td>" + numofinvprac + "</td><td>" + ACounter[ i ] + "</td><td>" + WCounter[ i ]+ "</td><td>" + PECounter[ i ]+ "</td><td>" + AACounter[ i ]+ "</td><td>" + result[ i ]+ "</td></tr>" ;
               } // end for
 
-            content += "</tbody></table>";
-            output2.innerHTML = content; // place the table in the output element
+        content += "</tbody></table>";
+        output2.innerHTML = content; // place the table in the output element        
     	} // end function outputTable
 
 
@@ -165,12 +200,12 @@ function runSim()
 	////////////////// Function for resetting all input fields and the output table //////////////////////////
   	function resetAll()
   		{
-    		document.getElementById('invnumofprac').value = '10';
-    		document.getElementById('threshold').value = '50';
+    		document.getElementById('invnumofprac').value = 10;
+    		document.getElementById('threshold').value = 50;
     		document.getElementById('numofsim').value = 1;
     		document.getElementById('numofpow').value = 0;
-        //document.getElementById('output1').innerHTML ="";
-    		document.getElementById('output2').innerHTML ="";
+        document.getElementById('output1').innerHTML = "";
+    		document.getElementById('output2').innerHTML = "";
   		}
 
 
