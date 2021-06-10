2b20704a273b708dc85338faf4cfdf1c9164521d
Samer Al-khateeb <sxalkhateeb@ualr.edu>
2021-06-10 15:04:38 -0500

    Code Update
    Updated code variables and comments at the top

--- MobSimulator-Python/Mob_Simulator-Q1.py
+++ MobSimulator-Python/Mob_Simulator-Q1.py
@@ -1,30 +1,31 @@
-# A program to simulate a CFM using Monte Carlo
+# A program to simulate a Mob using Monte Carlo
 # methods and the Theory of Collective Action.
 
 # By: Samer Al-khateeb
 
 # Q1: Given the following parameters (Number of Invited People,
 # Threshold Value of the Mob Success, the Number of Simulations (Mobs),
-# the Number of Powerful Actors) what is the chance a Mob will succeed?
+# the Number of Powerful Actors),
+# what is the chance a Mob will succeed?
 
 from random import randrange
 
 def getInputs():
-    CFM_Practitioners = int(input("Enter the Total Number of Invited CFM Practitioners: "))
-    Success_Threshold = float(input("Enter the Threshold Value for a CFM to Success: ")) / 100
-    Num_Of_Simulation = int(input("Enter the Number of Simulations (or Number of CFMs): "))
+    Mob_Practitioners = int(input("Enter the Total Number of Invited Mob Practitioners: "))
+    Success_Threshold = float(input("Enter the Threshold Value for a Mob to Success: ")) / 100
+    Num_Of_Simulation = int(input("Enter the Number of Simulations (or Number of Mobs): "))
     Num_Of_Powerful_Actors = int(input("Enter the Number of Powerful Actors: "))
 
-    return CFM_Practitioners, Success_Threshold, Num_Of_Simulation, Num_Of_Powerful_Actors
+    return Mob_Practitioners, Success_Threshold, Num_Of_Simulation, Num_Of_Powerful_Actors
 
-def SimulateOneCFM(CFM_Practitioners, Success_Threshold, Num_Powerful_Actors):
+def SimulateOneMob(Mob_Practitioners, Success_Threshold, Num_Powerful_Actors):
     Act_Counter = 0
     Withdraw_Counter = 0
     Power_Exchange_Counter = 0
     Act_Against_Counter = 0
     Participation_Rate = 0
     
-    for participant in range(CFM_Practitioners - Num_Powerful_Actors):
+    for participant in range(Mob_Practitioners - Num_Powerful_Actors):
         
         P_Interest = randrange(0,2)
         P_Control = randrange(0,2)
@@ -57,7 +58,7 @@ def SimulateOneCFM(CFM_Practitioners, Success_Threshold, Num_Powerful_Actors):
             else:
                 Act_Against_Counter = Act_Against_Counter + 1
             
-    Participation_Rate = ((Act_Counter + Num_Powerful_Actors) / CFM_Practitioners)
+    Participation_Rate = ((Act_Counter + Num_Powerful_Actors) / Mob_Practitioners)
 
     if (Participation_Rate >= Success_Threshold):
         return "Success", Num_Powerful_Actors, Act_Counter, Withdraw_Counter, Power_Exchange_Counter, Act_Against_Counter, Participation_Rate
@@ -65,39 +66,39 @@ def SimulateOneCFM(CFM_Practitioners, Success_Threshold, Num_Powerful_Actors):
         return "Fail", Num_Powerful_Actors, Act_Counter, Withdraw_Counter, Power_Exchange_Counter, Act_Against_Counter, Participation_Rate
 
 
-def SimulateManyCFM(Practitioners, Threshold, Num_Simulation, Num_Powerful_Actors):
-    num_Of_Success_CFM = 0
-    num_Of_Fail_CFM = 0
+def SimulateManyMob(Practitioners, Threshold, Num_Simulation, Num_Powerful_Actors):
+    num_Of_Success_Mob = 0
+    num_Of_Fail_Mob = 0
     Total_Participation_Rate = 0
 
     print("{0:<15} {1:^22} {2:^15} {3:^15} {4:^15} {5:^15} {6:^15}".format("Result", "Number of Powerful Actors", "Acted", "Withdraw", "Power Exchange", "Acat Against", "Participation Rate"))
     print("----------------------------------------------------------------------------------------------------------------------------")
     
-    for cfm in range(Num_Simulation):
-        result = SimulateOneCFM(Practitioners, Threshold, Num_Powerful_Actors)
+    for Mob in range(Num_Simulation):
+        result = SimulateOneMob(Practitioners, Threshold, Num_Powerful_Actors)
         
         if (result[0] == "Success"):
-            num_Of_Success_CFM = num_Of_Success_CFM + 1
+            num_Of_Success_Mob = num_Of_Success_Mob + 1
         elif (result[0] == "Fail"):
-            num_Of_Fail_CFM = num_Of_Fail_CFM + 1
+            num_Of_Fail_Mob = num_Of_Fail_Mob + 1
         print("{0:<15} {1:^22} {2:^15} {3:^15} {4:^15} {5:^15} {6:^15}".format(result[0], result[1], result[2], result[3], result[4], result[5], result[6]))
 
         Total_Participation_Rate = Total_Participation_Rate + result[6]
 
     Avg_Participation_Rate = Total_Participation_Rate/Num_Simulation
 
-    return num_Of_Success_CFM, num_Of_Fail_CFM, Avg_Participation_Rate
+    return num_Of_Success_Mob, num_Of_Fail_Mob, Avg_Participation_Rate
         
-def printStats(Num_Of_CFM_Success, Num_Of_CFM_Fail, Num_Simulation, Avg_Participation_Rate):
-    print("This program simulated ", Num_Simulation, "Cyber Flash Mobs")
-    print("The CFMs Success rate is ", round(((Num_Of_CFM_Success / Num_Simulation) * 100),2), "%")
-    print("The CFMs Fail rate is ", round(((Num_Of_CFM_Fail / Num_Simulation) * 100), 2), "%")
+def printStats(Num_Of_Mob_Success, Num_Of_Mob_Fail, Num_Simulation, Avg_Participation_Rate):
+    print("This program simulated ", Num_Simulation, "Mobs")
+    print("The Mobs Success rate is ", round(((Num_Of_Mob_Success / Num_Simulation) * 100),2), "%")
+    print("The Mobs Fail rate is ", round(((Num_Of_Mob_Fail / Num_Simulation) * 100), 2), "%")
     print("The Average Participation Rate is ", round(((Avg_Participation_Rate) * 100), 2), "%")
 
 
 def main():
     P, T_o, N_Sim, N_Pow_Act = getInputs()
-    Num_Of_CFM_Success, Num_Of_CFM_Fail, Avg_Participation_Rate = SimulateManyCFM(P, T_o, N_Sim, N_Pow_Act)
-    printStats(Num_Of_CFM_Success, Num_Of_CFM_Fail, N_Sim, Avg_Participation_Rate)
+    Num_Of_Mob_Success, Num_Of_Mob_Fail, Avg_Participation_Rate = SimulateManyMob(P, T_o, N_Sim, N_Pow_Act)
+    printStats(Num_Of_Mob_Success, Num_Of_Mob_Fail, N_Sim, Avg_Participation_Rate)
     
 main()
--- MobSimulator-Python/Mob_Simulator-Q2.py
+++ MobSimulator-Python/Mob_Simulator-Q2.py
@@ -1,20 +1,25 @@
-# A program to simulate a CFM using Monte Carlo
+# A program to simulate a Mob using Monte Carlo
 # methods and the Theory of Collective Action.
-#Q: Given the number of participants, how many powerful
-#   actors are needed to make a successful CFM?
+
+# By: Samer Al-khateeb
+
+# Q2: Given the following parameters (Number of Invited People,
+# Threshold Value of the Mob Success, and the Number of Simulations (Mobs)),
+# how many powerful actors are needed to have a successful mob?
+
 
 from random import randrange
 import math
 
 def getInputs():
-    CFM_Practitioners = int(input("Enter the Total Number of Invited CFM Practitioners: "))
-    Success_Threshold = float(input("Enter the Threshold Value for a CFM to Success: ")) 
-    Num_Of_Simulation = int(input("Enter the Number of Simulations (or Number of CFMs): "))
+    Mob_Practitioners = int(input("Enter the Total Number of Invited People: "))
+    Success_Threshold = float(input("Enter the Threshold Value for a Mob to Succeed: ")) 
+    Num_Of_Simulation = int(input("Enter the Number of Simulations (or Number of Mobs) you want to run: "))
 
-    return CFM_Practitioners, Success_Threshold, Num_Of_Simulation
+    return Mob_Practitioners, Success_Threshold, Num_Of_Simulation
 
 
-def SimulateOneCFM(CFM_Practitioners, Success_Threshold):
+def SimulateOneMob(Mob_Practitioners, Success_Threshold):
     Act_Counter = 0
     Withdraw_Counter = 0
     Power_Exchange_Counter = 0
@@ -25,7 +30,7 @@ def SimulateOneCFM(CFM_Practitioners, Success_Threshold):
     Needed_Powerful_Actors = 0
 
     #for each participant lets examing what (s)he will do
-    for participant in range(CFM_Practitioners):
+    for participant in range(Mob_Practitioners):
         
         P_Interest = randrange(0,2)
         P_Control = randrange(0,2)
@@ -58,57 +63,57 @@ def SimulateOneCFM(CFM_Practitioners, Success_Threshold):
             else:
                 Act_Against_Counter = Act_Against_Counter + 1
 
-    #once we examine each participant, lets see if the CFM succeed or Fail        
-    Participation_Rate = (Act_Counter / CFM_Practitioners)
+    #once we examine each participant, lets see if the Mob succeed or Fail        
+    Participation_Rate = (Act_Counter / Mob_Practitioners)
 
-    #if the participation rate is greater than the threshold, its a successful CFM
+    #if the participation rate is greater than the threshold, its a successful Mob
     if (Participation_Rate >= (Success_Threshold / 100)):
         return "Success", 0, Act_Counter, Withdraw_Counter, Power_Exchange_Counter, Act_Against_Counter, Participation_Rate
 
-    #else its a fail CFM and we need to calculate how much was needed to be successful
+    #else its a fail Mob and we need to calculate how much was needed to be successful
     else:
-        Needed_Powerful_Actors = (((Success_Threshold * CFM_Practitioners ) / 100)) - Act_Counter
+        Needed_Powerful_Actors = (((Success_Threshold * Mob_Practitioners ) / 100)) - Act_Counter
         return "Fail", Needed_Powerful_Actors, Act_Counter, Withdraw_Counter, Power_Exchange_Counter, Act_Against_Counter, Participation_Rate
 
 
-def SimulateManyCFM(Practitioners, Threshold, Num_Simulation):
-    num_Of_Success_CFM = 0
-    num_Of_Fail_CFM = 0
+def SimulateManyMob(Practitioners, Threshold, Num_Simulation):
+    num_Of_Success_Mob = 0
+    num_Of_Fail_Mob = 0
 
     needed = 0
 
     print("{0:<15} {1:^22} {2:^15} {3:^15} {4:^15} {5:^15} {6:^15}".format("Result", "Needed Powerful Actors", "Acted", "Withdraw", "Power Exchange", "Acat Against", "Participation Rate"))
     print("-------------------------------------------------------------------------------------------------------------------------")
 
-    #lets simulate many CFMs
-    for cfm in range(Num_Simulation):
+    #lets simulate many Mobs
+    for Mob in range(Num_Simulation):
         #by calling simulate one multiple times
-        result = SimulateOneCFM(Practitioners, Threshold)
+        result = SimulateOneMob(Practitioners, Threshold)
         #based on result we can check how many succeded, failed, and what was the needed Powerful Actors
         if (result[0] == "Success"):
-            num_Of_Success_CFM = num_Of_Success_CFM + 1
+            num_Of_Success_Mob = num_Of_Success_Mob + 1
             needed = needed + 0
         else:
-            num_Of_Fail_CFM = num_Of_Fail_CFM + 1
+            num_Of_Fail_Mob = num_Of_Fail_Mob + 1
             needed = needed + float(result[1])
         print("{0:<15} {1:^22} {2:^15} {3:^15} {4:^15} {5:^15} {6:^15}".format(result[0], result[1], result[2], result[3], result[4], result[5], result[6]))
 
-    Average_Needed_Powerful_Actors = math.ceil(needed/num_Of_Fail_CFM)
+    Average_Needed_Powerful_Actors = math.ceil(needed/num_Of_Fail_Mob)
 
-    return num_Of_Success_CFM, num_Of_Fail_CFM, Average_Needed_Powerful_Actors
+    return num_Of_Success_Mob, num_Of_Fail_Mob, Average_Needed_Powerful_Actors
 
-def printStats(Num_Of_CFM_Success, Num_Of_CFM_Fail, Num_Simulation, Average_Needed_Powerful_Actors):
-    print("This program simulated ", Num_Simulation, "Cyber Flash Mobs")
-    print("The CFMs Success rate is ", round(((Num_Of_CFM_Success / Num_Simulation) * 100),2), "%")
-    print("The CFMs Fail rate is ", round(((Num_Of_CFM_Fail / Num_Simulation) * 100), 2), "%")
+def printStats(Num_Of_Mob_Success, Num_Of_Mob_Fail, Num_Simulation, Average_Needed_Powerful_Actors):
+    print("This program simulated ", Num_Simulation, "Mobs")
+    print("The Mobs Success rate is ", round(((Num_Of_Mob_Success / Num_Simulation) * 100),2), "%")
+    print("The Mobs Fail rate is ", round(((Num_Of_Mob_Fail / Num_Simulation) * 100), 2), "%")
     print("On Average you need at least", Average_Needed_Powerful_Actors, " more Powerful Actors to Succeed")
     
 
 
 def main():
     P, T_o, N_Sim = getInputs()
-    Num_Of_CFM_Success, Num_Of_CFM_Fail, Needed = SimulateManyCFM(P, T_o, N_Sim)
-    printStats(Num_Of_CFM_Success, Num_Of_CFM_Fail, N_Sim, Needed)
+    Num_Of_Mob_Success, Num_Of_Mob_Fail, Needed = SimulateManyMob(P, T_o, N_Sim)
+    printStats(Num_Of_Mob_Success, Num_Of_Mob_Fail, N_Sim, Needed)
     
 main()
 
